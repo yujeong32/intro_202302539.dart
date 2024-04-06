6193e1684b249fc0ab0adc7a6f4c22ad9a9a8a73
hyechan23 <heechans223@gmail.com>
2024-04-06 23:07:18 +0900

    second

--- test/basic_project_test.dart
+++ test/basic_project_test.dart
@@ -2,6 +2,7 @@ import 'package:basic_project/basic_project.dart';
 import 'package:test/test.dart';
 
 void main() {
-  print("202302546\n");
-  print("김희찬");
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
 }
